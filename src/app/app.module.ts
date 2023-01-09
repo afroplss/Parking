036adc9de77a9310739195fc919a418cb32273df
afroplss <joi.munozm@duocuc.cl>
2023-01-09 20:22:38 -0300

    geolocalizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { Geolocation } from '@ionic-native/geolocation';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  declarations: [],
+  imports: [ ],
+  bootstrap: [IonicApp],
+  entryComponents: [],
+  providers: [
+
+    Geolocation,
+    
+  ]
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
+
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 
 @Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public geolocation:Geolocation) {
 
 }
